868577d921eb6ff8fa453b370e63433af84d74bb
neurobomberhikes <neurobomberhikes@gmail.com>
2026-07-16 13:20:41 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pct-full-v19';
+const CACHE = 'pct-full-v20';
 const ASSETS = ['./', './index.html', './icon-180.png', './icon-512.png', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
